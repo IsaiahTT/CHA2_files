3eecbe52c9fd9221e5f0d01e803f5869410572fb
IsaiahTheMag <isaiahtensae@gmail.com>
2021-12-07 15:38:50 -0800

    save_csv function added

--- Starter_Code/loan_qualifier_app/app.py
+++ Starter_Code/loan_qualifier_app/app.py
@@ -101,6 +101,18 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
 
     return bank_data_filtered
 
+def save_csv(qualifying_loans):
+    csv_loan_path = Path("qualifying_loans.csv")
+    with open(csv_loan_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        # Header row
+        csvwriter.writerow(header)
+
+        # Data rows
+        for row in qualifying_loans:
+            csvwriter.writerow(row.values())
+
 
 def save_qualifying_loans(qualifying_loans):
     """Saves the qualifying loans to a CSV file.
